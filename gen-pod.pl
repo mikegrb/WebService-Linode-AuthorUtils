90df6436a5e3724c1ba6a425e2166a70ae90a358
mikegrb <mgreb@linode.com>
2013-09-25 15:13:57 -0400

    Clean up POD a bit remove required/optional parameter headings when list is empty
    I forgot how ugly this code is, needs a rewrite with templates or something

--- gen-pod.pl
+++ gen-pod.pl
@@ -148,13 +148,17 @@ foreach my $group (qw{avail domain domain_resource linode linode_config linode_d
     # print "=head2 $group\n\n";
     foreach my $method (keys %{$validation{$group}}) {
         print "=head3 ${group}_${method}\n\n";
-        print "Required Parameters:\n\n";
-        print "=over 4\n\n";
-        print "=item * $_\n\n" for @{$validation{$group}{$method}[0]};
-        print "=back\n\n";
-        print "Optional Parameters:\n\n";
-        print "=over 4\n\n";
-        print "=item * $_\n\n" for @{$validation{$group}{$method}[1]};
-        print "=back\n\n";
+        if (@{$validation{$group}{$method}[0]}) {
+            print "Required Parameters:\n\n";
+            print "=over 4\n\n";
+            print "=item * $_\n\n" for @{$validation{$group}{$method}[0]};
+            print "=back\n\n";
+        }
+        if (@{$validation{$group}{$method}[1]}) {
+            print "Optional Parameters:\n\n";
+            print "=over 4\n\n";
+            print "=item * $_\n\n" for @{$validation{$group}{$method}[1]};
+            print "=back\n\n";
+        }
     }
 }
