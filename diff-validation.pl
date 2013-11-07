f90b089112e0e970a03c908958bca67447775cd2
mikegrb <mgreb@linode.com>
2013-11-07 12:03:40 -0500

    update validation hash

--- diff-validation.pl
+++ diff-validation.pl
@@ -60,99 +60,6 @@ for my $group ( sort keys $new ) {
 sub get_old_new {
     my $old_validation = {
         account => {
-            info => [ [], [] ],
-        },
-        api => {
-            spec => [ [], [] ],
-        },
-        avail => {
-            datacenters => [ [], [] ],
-            distributions => [ [], [ 'distributionid' ] ],
-            kernels => [ [], [ 'isxen', 'kernelid' ] ],
-            linodeplans => [ [ 'planid' ], [] ],
-            stackscripts => [ [], [qw( keywords distributionid distributionvendor )] ],
-        },
-        domain => {
-            create => [ [ 'domain', 'type' ], [qw( status ttl_sec expire_sec master_ips refresh_sec soa_email retry_sec axfr_ips description )] ],
-            delete => [ [ 'domainid' ], [] ],
-            list => [ [], [ 'domainid' ] ],
-            update => [ [ 'domainid' ], [qw( status domain ttl_sec expire_sec type master_ips refresh_sec soa_email axfr_ips retry_sec description )] ],
-        },
-        domain_resource => {
-            create => [ [qw( domainid type )], [qw( target ttl_sec port weight priority protocol name )] ],
-            delete => [ [ 'resourceid', 'domainid' ], [] ],
-            list => [ [ 'domainid' ], [ 'resourceid' ] ],
-            update => [ [ 'resourceid' ], [qw( target domainid ttl_sec port weight priority protocol name )] ],
-        },
-        linode => {
-            boot => [ [ 'linodeid' ], [ 'configid' ] ],
-            clone => [ [qw( planid paymentterm linodeid datacenterid )], [] ],
-            create => [ [qw( planid paymentterm datacenterid )], [] ],
-            delete => [ [ 'linodeid' ], [ 'skipchecks' ] ],
-            list => [ [], [ 'linodeid' ] ],
-            reboot => [ [ 'linodeid' ], [ 'configid' ] ],
-            resize => [ [ 'planid', 'linodeid' ], [] ],
-            shutdown => [ [ 'linodeid' ], [] ],
-            update => [ [ 'linodeid' ], [qw( alert_bwquota_threshold alert_bwin_threshold alert_cpu_threshold alert_cpu_enabled alert_diskio_enabled label backupweeklyday alert_bwquota_enabled watchdog lpm_displaygroup alert_bwin_enabled alert_bwout_enabled alert_bwout_threshold alert_diskio_threshold backupwindow )] ],
-        },
-        linode_config => {
-            create => [ [qw( linodeid label kernelid )], [qw( comments helper_xen devtmpfs_automount rootdevicecustom rootdevicero helper_depmod helper_disableupdatedb rootdevicenum disklist runlevel ramlimit )] ],
-            delete => [ [ 'configid', 'linodeid' ], [] ],
-            list => [ [ 'linodeid' ], [ 'configid' ] ],
-            update => [ [ 'configid' ], [qw( comments helper_xen devtmpfs_automount rootdevicecustom linodeid rootdevicero label helper_depmod helper_disableupdatedb rootdevicenum disklist runlevel kernelid ramlimit )] ],
-        },
-        linode_disk => {
-            create => [ [qw( type size linodeid label )], [] ],
-            createfromdistribution => [ [qw( size rootpass linodeid distributionid label )], [ 'rootsshkey' ] ],
-            createfromstackscript => [ [qw( size linodeid rootpass distributionid stackscriptudfresponses stackscriptid label )], [] ],
-            delete => [ [ 'diskid', 'linodeid' ], [] ],
-            duplicate => [ [ 'diskid', 'linodeid' ], [] ],
-            list => [ [ 'linodeid' ], [ 'diskid' ] ],
-            resize => [ [qw( diskid linodeid size )], [] ],
-            update => [ [ 'diskid' ], [qw( linodeid isreadonly label )] ],
-        },
-        linode_ip => {
-            addprivate => [ [ 'linodeid' ], [] ],
-            list => [ [ 'linodeid' ], [ 'ipaddressid' ] ],
-        },
-        linode_job => {
-            list => [ [ 'linodeid' ], [ 'pendingonly', 'jobid' ] ],
-        },
-        nodebalancer => {
-            create => [ [ 'paymentterm', 'datacenterid' ], [ 'label', 'clientconnthrottle' ] ],
-            delete => [ [ 'nodebalancerid' ], [] ],
-            list => [ [], [ 'nodebalancerid' ] ],
-            update => [ [ 'nodebalancerid' ], [ 'label', 'clientconnthrottle' ] ],
-        },
-        nodebalancer_config => {
-            create => [ [ 'nodebalancerid' ], [qw( check_path check_body stickiness port check check_timeout check_attempts check_interval protocol algorithm )] ],
-            delete => [ [ 'configid' ], [] ],
-            list => [ [ 'nodebalancerid' ], [ 'configid' ] ],
-            update => [ [ 'configid' ], [qw( check_path check_body stickiness port check_timeout check check_attempts check_interval protocol algorithm )] ],
-        },
-        nodebalancer_node => {
-            create => [ [qw( configid address label )], [ 'mode', 'weight' ] ],
-            delete => [ [ 'nodeid' ], [] ],
-            list => [ [ 'configid' ], [ 'nodeid' ] ],
-            update => [ [ 'nodeid' ], [qw( address mode weight label )] ],
-        },
-        stackscript => {
-            create => [ [qw( script distributionidlist label )], [qw( rev_note description ispublic )] ],
-            delete => [ [ 'stackscriptid' ], [] ],
-            list => [ [], [ 'stackscriptid' ] ],
-            update => [ [ 'stackscriptid' ], [qw( script rev_note ispublic label distributionidlist description )] ],
-        },
-        test => {
-            echo => [ [], [] ],
-        },
-        user => {
-            getapikey => [ [ 'password', 'username' ], [] ],
-        },
-    };
-
-    my $new_validation = {
-
-        account => {
             estimateinvoice => [ ['mode'], [qw( paymentterm linodeid planid )] ],
             info            => [ [],       [] ],
             paybalance      => [ [],       [] ],
@@ -293,6 +200,104 @@ sub get_old_new {
 
 
     };
+
+    my $new_validation = {
+        account => {
+            estimateinvoice => [ [ 'mode' ], [qw( linodeid paymentterm planid )] ],
+            info => [ [], [] ],
+            paybalance => [ [], [] ],
+            updatecard => [ [qw( ccexpmonth ccexpyear ccnumber )], [] ],
+        },
+        api => {
+            spec => [ [], [] ],
+        },
+        avail => {
+            datacenters => [ [], [] ],
+            distributions => [ [], [ 'distributionid' ] ],
+            kernels => [ [], [ 'isxen', 'kernelid' ] ],
+            linodeplans => [ [], [ 'planid' ] ],
+            stackscripts => [ [], [qw( distributionid distributionvendor keywords )] ],
+        },
+        domain => {
+            create => [ [ 'domain', 'type' ], [qw( axfr_ips description expire_sec lpm_displaygroup master_ips refresh_sec retry_sec soa_email status ttl_sec )] ],
+            delete => [ [ 'domainid' ], [] ],
+            list => [ [], [ 'domainid' ] ],
+            update => [ [ 'domainid' ], [qw( axfr_ips description domain expire_sec lpm_displaygroup master_ips refresh_sec retry_sec soa_email status ttl_sec type )] ],
+        },
+        domain_resource => {
+            create => [ [qw( domainid type )], [qw( name port priority protocol target ttl_sec weight )] ],
+            delete => [ [ 'domainid', 'resourceid' ], [] ],
+            list => [ [ 'domainid' ], [ 'resourceid' ] ],
+            update => [ [ 'resourceid' ], [qw( domainid name port priority protocol target ttl_sec weight )] ],
+        },
+        linode => {
+            boot => [ [ 'linodeid' ], [ 'configid' ] ],
+            clone => [ [qw( datacenterid linodeid paymentterm planid )], [] ],
+            create => [ [qw( datacenterid paymentterm planid )], [] ],
+            delete => [ [ 'linodeid' ], [ 'skipchecks' ] ],
+            list => [ [], [ 'linodeid' ] ],
+            mutate => [ [ 'linodeid' ], [] ],
+            reboot => [ [ 'linodeid' ], [ 'configid' ] ],
+            resize => [ [ 'linodeid', 'planid' ], [] ],
+            shutdown => [ [ 'linodeid' ], [] ],
+            update => [ [ 'linodeid' ], [qw( alert_bwin_enabled alert_bwin_threshold alert_bwout_enabled alert_bwout_threshold alert_bwquota_enabled alert_bwquota_threshold alert_cpu_enabled alert_cpu_threshold alert_diskio_enabled alert_diskio_threshold backupweeklyday backupwindow label lpm_displaygroup ms_ssh_disabled ms_ssh_ip ms_ssh_port ms_ssh_user watchdog )] ],
+            webconsoletoken => [ [ 'linodeid' ], [] ],
+        },
+        linode_config => {
+            create => [ [qw( kernelid label linodeid )], [qw( comments devtmpfs_automount disklist helper_depmod helper_disableupdatedb helper_xen ramlimit rootdevicecustom rootdevicenum rootdevicero runlevel )] ],
+            delete => [ [ 'configid', 'linodeid' ], [] ],
+            list => [ [ 'linodeid' ], [ 'configid' ] ],
+            update => [ [ 'configid' ], [qw( comments devtmpfs_automount disklist helper_depmod helper_disableupdatedb helper_xen kernelid label linodeid ramlimit rootdevicecustom rootdevicenum rootdevicero runlevel )] ],
+        },
+        linode_disk => {
+            create => [ [qw( label linodeid size type )], [] ],
+            createfromdistribution => [ [qw( distributionid label linodeid rootpass size )], [ 'rootsshkey' ] ],
+            createfromstackscript => [ [qw( distributionid label linodeid rootpass size stackscriptid stackscriptudfresponses )], [] ],
+            delete => [ [ 'diskid', 'linodeid' ], [] ],
+            duplicate => [ [ 'diskid', 'linodeid' ], [] ],
+            list => [ [ 'linodeid' ], [ 'diskid' ] ],
+            resize => [ [qw( diskid linodeid size )], [] ],
+            update => [ [ 'diskid' ], [qw( isreadonly label linodeid )] ],
+        },
+        linode_ip => {
+            addprivate => [ [ 'linodeid' ], [] ],
+            list => [ [ 'linodeid' ], [ 'ipaddressid' ] ],
+        },
+        linode_job => {
+            list => [ [ 'linodeid' ], [ 'jobid', 'pendingonly' ] ],
+        },
+        nodebalancer => {
+            create => [ [ 'datacenterid', 'paymentterm' ], [ 'clientconnthrottle', 'label' ] ],
+            delete => [ [ 'nodebalancerid' ], [] ],
+            list => [ [], [ 'nodebalancerid' ] ],
+            update => [ [ 'nodebalancerid' ], [ 'clientconnthrottle', 'label' ] ],
+        },
+        nodebalancer_config => {
+            create => [ [ 'nodebalancerid' ], [qw( algorithm check check_attempts check_body check_interval check_path check_timeout port protocol ssl_cert ssl_key stickiness )] ],
+            delete => [ [ 'configid' ], [] ],
+            list => [ [ 'nodebalancerid' ], [ 'configid' ] ],
+            update => [ [ 'configid' ], [qw( algorithm check check_attempts check_body check_interval check_path check_timeout port protocol ssl_cert ssl_key stickiness )] ],
+        },
+        nodebalancer_node => {
+            create => [ [qw( address configid label )], [ 'mode', 'weight' ] ],
+            delete => [ [ 'nodeid' ], [] ],
+            list => [ [ 'configid' ], [ 'nodeid' ] ],
+            update => [ [ 'nodeid' ], [qw( address label mode weight )] ],
+        },
+        stackscript => {
+            create => [ [qw( distributionidlist label script )], [qw( description ispublic rev_note )] ],
+            delete => [ [ 'stackscriptid' ], [] ],
+            list => [ [], [ 'stackscriptid' ] ],
+            update => [ [ 'stackscriptid' ], [qw( description distributionidlist ispublic label rev_note script )] ],
+        },
+        test => {
+            echo => [ [], [] ],
+        },
+        user => {
+            getapikey => [ [ 'password', 'username' ], [] ],
+        },
+    };
+
 
     return $old_validation, $new_validation;
 }
