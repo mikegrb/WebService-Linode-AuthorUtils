c914734437ed315d3564722290d7cd175989cacf
Michael Greb <michael@thegrebs.com>
2015-01-10 23:25:37 -0500

    support outputting concise or verbose versions of the spec

--- gen-validation.pl
+++ gen-validation.pl
@@ -2,28 +2,38 @@
 
 use strict;
 use warnings;
+use 5.014;
 
 use Mojo::UserAgent;
 use Data::Dumper;
+use Getopt::Std;
 
-my $api_base   = shift ||  "https://api.linode.com/";
-my $spec       = get_spec();
-my $validation = parse_spec($spec);
+my %opts;
+getopts( 'v', \%opts );
 
-print Data::Dumper->Dump( [ $validation ], [ 'verbose_validation' ] );
+my $api_base = shift || "https://api.linode.com/";
+my $spec = get_spec();
+my ( $verbose_validation, $validation ) = parse_spec($spec);
+
+if ( $opts{v} ) {
+  print Data::Dumper->Dump( [$verbose_validation], ['verbose_validation'] );
+}
+else {
+  dump_validation($validation);
+}
 
 sub get_spec {
     my $res = Mojo::UserAgent->new->get(
         "${api_base}?api_action=api.spec")->res;
-    die $res->error->{message} if $res->error;
-    return $res;
+  die $res->error->{message} if $res->error;
+  return $res;
 }
 
 sub parse_spec {
     my $res     = shift;
     my $methods = $res->json->{DATA}->{METHODS};
 
-    my $validation;
+    my ( $verbose_validation, $valiation );
     for my $remote_method ( keys %$methods ) {
         unless ( $remote_method =~ m/^(\S+)\.([^\.\s]++)$/ ) {
             warn "UNMATCHED METHOD: $remote_method\n";
@@ -34,12 +44,16 @@ sub parse_spec {
 
         for my $parameter ( keys %{ $methods->{$remote_method}{PARAMETERS} } ) {
             my $key = $methods->{$remote_method}{PARAMETERS}{$parameter}{REQUIRED} ? 'required' : 'optional';
-            push @{ $validation->{$group}{$method}{$key} },
+            push @{ $verbose_validation->{$group}{$method}{$key} },
                 [ lc $parameter, $methods->{$remote_method}{PARAMETERS}{$parameter}{DESCRIPTION} ];
         }
-        $validation->{$group}{$method}{description} =$methods->{$remote_method}{DESCRIPTION};
+        for my $type ( 'required', 'optional') {
+          push @{ $validation->{$group}{$method} }, [ map { $_->[0] } @{ $verbose_validation->{$group}{$method}{$type} } ];
+        }
+
+        $verbose_validation->{$group}{$method}{description} = $methods->{$remote_method}{DESCRIPTION};
     }
-    return $validation;
+    return $verbose_validation, $validation;
 }
 
 sub dump_validation {
@@ -49,8 +63,7 @@ sub dump_validation {
         print "    $group => {\n";
         for my $method ( sort keys %{ $validation->{$group} } ) {
             my $args = join ', ',
-                map { ref $_ ? dump_arrayref($_) : qq{"$_"} } { $validation->{$group}{$method} };
-
+                map { dump_arrayref($_) } @{ $validation->{$group}{$method} };
             print "        $method => [ $args ],\n";
         }
         print "    },\n";
