c3cec8acbfcf7e0387332c598ac6855f9edc5dd4
Mike Greb <michael@thegrebs.com>
2019-09-05 01:21:45 -0400

    update diff-validation for newer perl

--- diff-validation.pl
+++ diff-validation.pl
@@ -14,55 +14,53 @@ my $changes = 0;
 # don't eval untrusted shit, m'kay?
 eval '$_ = ' . read_file($_) for ( $old, $new );
 
-for my $group ( sort keys $old ) {
-    unless ( exists $new->{$group} ) {
-        say "Group $group no longer exists.\n";
-        $changes++;
-        next;
-    }
+for my $group ( sort keys %$old ) {
+	unless ( exists $new->{$group} ) {
+		say "Group $group no longer exists.\n";
+		$changes++;
+		next;
+	}
 
-    for my $method ( sort keys $old->{$group} ) {
-        unless ( exists $new->{$group}{$method} ) {
-            say "Group $group.$method no longer exists.\n";
-            $changes++;
-            next;
-        }
+	for my $method ( sort keys %{$old->{$group}} ) {
+		unless ( exists $new->{$group}{$method} ) {
+			say "Group $group.$method no longer exists.\n";
+			$changes++;
+			next;
+		}
 
-        my $old_args = $old->{$group}{$method};
-        my $new_args = $new->{$group}{$method};
+		my $old_args = $old->{$group}{$method};
+		my $new_args = $new->{$group}{$method};
 
-        my $args = {
-            'required' => List::Compare->new( $old_args->[0], $new_args->[0] ),
-            'optional' => List::Compare->new( $old_args->[1], $new_args->[1] )
-        };
+		my $args = {
+			'required' => List::Compare->new( $old_args->[0], $new_args->[0] ),
+			'optional' => List::Compare->new( $old_args->[1], $new_args->[1] )};
 
-        for my $type ( keys $args ) {
-            next if $args->{$type}->is_LequivalentR();
-            say "$group.$method difference in $type arguments: ";
-            $changes++;
-            my @old = $args->{$type}->get_unique();
-            print "\t";
-            for my $differing ( $args->{$type}->get_symmetric_difference() ) {
-                my $removed = grep { $_ eq $differing } @old;
-                print colored( ( $removed ? '- ' : '+ ' ) . $differing . '  ',
-                    ( $removed ? 'red' : 'green' ) );
-            }
-            say "\n";
-        }
-    }
+		for my $type ( keys %$args ) {
+			next if $args->{$type}->is_LequivalentR();
+			say "$group.$method difference in $type arguments: ";
+			$changes++;
+			my @old = $args->{$type}->get_unique();
+			print "\t";
+			for my $differing ( $args->{$type}->get_symmetric_difference() ) {
+				my $removed = grep { $_ eq $differing } @old;
+				print colored( ( $removed ? '- ' : '+ ' ) . $differing . '  ',( $removed ? 'red' : 'green' ) );
+			}
+			say "\n";
+		}
+	}
 
-    for my $method ( sort keys $new->{$group} ) {
-        next if exists $old->{$group}{$method};
-        say "New method $group.$method\n";
-        $changes++;
-    }
+	for my $method ( sort keys %{$new->{$group}} ) {
+		next if exists $old->{$group}{$method};
+		say "New method $group.$method\n";
+		$changes++;
+	}
 
 }
 
-for my $group ( sort keys $new ) {
-    next if exists $old->{$group};
-    say "New group $group\n";
-    $changes++;
+for my $group ( sort keys %$new ) {
+	next if exists $old->{$group};
+	say "New group $group\n";
+	$changes++;
 }
 
 exit $changes;
